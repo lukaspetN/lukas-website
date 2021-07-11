c9cbe4b3cfc7a6ca7b0c995df8f67b0bb8104f94
lukaspetN <87276107+lukaspetN@users.noreply.github.com>
2021-07-12 01:28:24 +0800

    Lukas coming soon

---
+++ main.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    const cursor = document.querySelector('.cursor');
+
+
+    document.addEventListener('mousemove', e => {
+      cursor.setAttribute('style', 'top: ' + (e.pageY - 75) + 'px; left: ' + (e.pageX- 155) + 'px;') 
+    })
+    // cursor image is 310 x 150; so (e.pageY - 75) bec 150/2 and
+    // 310/2 = 155 para gitna ng image yung mouse
+
+
+    // var $wrap = $('.wrapper'),
+    var eyes = $('.doggo-eyes'),
+    followX = 0,
+    followY = 0,
+    x = 0,
+    y = 0,
+    friction = 1 / 12;
+  
+  function animate() {
+      x += (followX - x) * friction;
+      y += (followY - y) * friction;
+  
+      eyes.css({
+          '-webit-transform': 'translate(' + -x + 'px, ' + -y + 'px)',
+          '-moz-transform': 'translate(' + -x + 'px, ' + -y + 'px)',
+          'transform': 'translate(' + -x + 'px, ' + -y + 'px)'
+      });
+
+      window.requestAnimationFrame(animate);
+  }
+  
+  $(window).on('mousemove', function (e) {
+      var mouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+      var mouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+      followX = (12 * mouseX) / 125; 
+      followY = (10 * mouseY) / 125;
+  });
+  
+  animate();
+});
+
